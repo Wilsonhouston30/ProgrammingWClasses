648d57a09230016ecc2e9fecf515afd138447390
Wilsonhouston30 <hou22004@byui.edu>
2025-10-01 00:43:01 -0600

    PrepareEncap

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+
+        Fraction wholenumber = new Fraction(1);
+        Console.WriteLine(wholenumber.GetDecimalValue());
+        Console.WriteLine(wholenumber.GetFraction());
+        Fraction wholenumber1 = new Fraction(6,1);
+        Console.WriteLine(wholenumber1.GetDecimalValue());
+        Console.WriteLine(wholenumber1.GetFraction());
+        Fraction wholenumber2 = new Fraction(1,3);
+        Console.WriteLine(wholenumber2.GetDecimalValue());
+        Console.WriteLine(wholenumber2.GetFraction());
     }
 }
