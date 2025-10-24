f60ae49f496655992b0fa5fdd7764fc0a24324a7
Wilsonhouston30 <hou22004@byui.edu>
2025-10-23 21:46:35 -0600

    PolyPrepCompleted

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        Console.WriteLine("Polymorphism prep");
+
+        Square Square1 = new Square("blue", 2.5);
+        Console.WriteLine(Square1.GetColor());
+        Console.WriteLine(Square1.GetArea());
+
+        Rectangle rectangle1 = new Rectangle(5,5,"blue");
+        Console.WriteLine(rectangle1.GetColor());
+        Console.WriteLine(rectangle1.GetArea());
+
+
     }
 }
